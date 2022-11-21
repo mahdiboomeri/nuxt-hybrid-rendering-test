8e40f8f2e5a95b822a4bb90f75190af52f8920b4
Mahdi Boomeri <mahdi.b2or@gmail.com>
2022-11-22 00:06:32 +0330

    swr with correct revalidate max-age

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   routeRules: {
     // Fully static on build (using wildcards)
-    "/blog/**": { swr: true },
+    "/blog/**": { swr: 60 * 1000 },
     // CSR
     "/client": { ssr: false },
     // Static on build
@@ -10,6 +10,6 @@ export default defineNuxtConfig({
     // Redirect to /ssr
     "/redirectToSSR": { redirect: "/ssr" },
     // On-damand
-    "/swr": { swr: 60 },
+    "/swr": { swr: 6000 },
   },
 });
